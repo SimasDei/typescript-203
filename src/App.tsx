8c41478ac91be9472790a9d38cd488bfe7b0a856
Simas <simas@simas-dei.com>
2019-12-31 19:07:00 +0200

    implementing interfaces for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import ToDoList from './components/ToDoList';
+
+const todos = [
+  {
+    id: '1',
+    text: 'Todo Text',
+  },
+];
+
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <ToDoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
